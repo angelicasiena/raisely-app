a10da641d717c789a5815511323261a24fb3c963
Siena Abaya <siena.abaya@rocketmiles.com>
2020-09-08 01:19:55 +0800

    Add message component

---
+++ src/message/message.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box } from "../styled-tags/index";
+import { SignupMessage } from "../types/index";
+
+const colors = {
+  success: {
+    backgroundColor: "#ceead5",
+    color: "#154d21",
+  },
+  error: {
+    backgroundColor: "#f7d1d5",
+    color: "#671a21",
+  },
+};
+
+export const MessageBox: React.FC<SignupMessage> = (
+  messageData: SignupMessage
+) => {
+  return (
+    <Box
+      bg={colors[messageData.status]?.backgroundColor}
+      color={colors[messageData.status]?.color}
+    >
+      {messageData.message}
+    </Box>
+  );
+};
